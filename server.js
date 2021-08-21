dd268213363ca362ca0759143ad91c77a4d48819
Vankata999 <80715460+Vankata999@users.noreply.github.com>
2021-08-21 12:40:39 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,68 +1,59 @@
- const express = require('express')
- const app = express()
-
-const { Server } = require('socket.io')
-
+const express = require('express')
+const app = express()
 const server = require('http').Server(app)
-const io = require('socket.io')(server)//server)
+const io = require('socket.io')(server)
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-const rooms = {}
+const rooms = { }
 
-app.get('/',(req, res ) =>{
-    res.render('index', {rooms: rooms})
+app.get('/', (req, res) => {
+  res.render('index', { rooms: rooms })
+}) // local host home page
+
+app.post('/room', (req, res) => {
+  if (rooms[req.body.room] != null) {
+    return res.redirect('/')
+  }// вкарва инфо в базата
+  rooms[req.body.room] = { users: {} }
+  res.redirect(req.body.room)
+  // Send message that new room was created
+  io.emit('room-created', req.body.room)
 })
 
-app.post('/room', (req,res) =>{
-    if(rooms[req.body.room] != null){
-        return res.redirect('/')
-    }
-    rooms[req.body.room] = {users:{}}
-    res.redirect(req.body.room)
-    //send message that new room was created
-    io.emit('room-created', req.body.room)
-})
-
-app.get('/:room', (req,res) => {
-    if(rooms[req.params.room] === null){
-        return res.redirect('/')
-    }
-    res.render('room',{roomName: req.params.room})
+app.get('/:room', (req, res) => { //рекуест заявка към сървъра
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/')
+  }
+  res.render('room', { roomName: req.params.room })// ако не вкарай ме в стаята
 })
 
 server.listen(3000)
 
+io.on('connection', socket => {
+  socket.on('new-user', (room, name) => {
+    socket.join(room) // вържи се към стаята 
+    rooms[room].users[socket.id] = name // вземам id  на човека да няма с еднакво име
+    socket.to(room).broadcast.emit('user-connected', name) // казва на вс юзъри че няксой е взязъл
 
-
-io.on('connection',socket => {
-    socket.on('new-user',(room,name) =>{
-        socket.join(room)
-        rooms[room].users[socket.id] = name
-        socket.to(room).broadcast.emit('user-connected')
+  })
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })//изпрати в стаята и му вземи името по id
+  })
+  socket.on('disconnect', () => {
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id] // казваш на всички че някой е напуснал и го махаме от стаята
     })
-    socket.on('send-chat-message' ,(room, message) =>{
-        socket.to(room).broadcast.emit('chat-message',{message: message,
-             name: rooms[room].users[socket.id]})
-    })
-    
-        socket.on('disconnect', () =>{
-           getUserRooms(socket).forEach(room => {
-            socket.to(room).broadcast.emit('user-disconnected', 
-            rooms[room].users[socket.id])
-            delete users[socket.id]
-           })
-            
-        })
+  })
 })
 
-//convert object ro array
-function getUserRooms(socket) {
-    return Object.entries(rooms).reduce((names,[name, room] ) =>{
-        if(room.users[socket.id] != null) names.push(name)
-        return names
-    }, [])
+function getUserRooms(socket) { // дава ми всички имена в стаята
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
 }
